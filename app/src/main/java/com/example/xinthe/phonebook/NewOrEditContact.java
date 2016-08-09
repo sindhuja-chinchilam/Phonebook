fc1a111b41fe7673aa023fb38e9deb7b8750615b
sindhuja <chinchilamsindhuja@gmail.com>
2016-08-09 17:00:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/xinthe/phonebook/NewOrEditContact.java
@@ -0,0 +1,13 @@
+package com.example.xinthe.phonebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewOrEditContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_or_edit_contact);
+    }
+}
